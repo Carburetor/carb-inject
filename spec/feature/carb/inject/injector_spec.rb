316b8ad0787212b70751ab0ef89966a90f2df6ef
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2016-11-30 10:10:05 -0800

    Added test for overwriting dependencies

--- spec/feature/carb/inject/injector_spec.rb
+++ spec/feature/carb/inject/injector_spec.rb
@@ -21,4 +21,23 @@ describe Carb::Inject::Injector, type: :feature do
 
     expect(instance.output).to eq "123 test"
   end
+
+  it "overwrite dependencies with passed arguments" do
+    container = Carb::SimpleContainer.new({
+      :foo       => -> { 123 },
+      "carb.bar" => -> { "test" }
+    })
+    injector = Carb::Inject::Injector.new(container)
+    klass = Class.new do
+      include injector[:foo, bar: "carb.bar"]
+
+      def output
+        "#{foo} #{bar}"
+      end
+    end
+
+    instance = klass.new(foo: "blah")
+
+    expect(instance.output).to eq "blah test"
+  end
 end
